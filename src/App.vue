6d0e37da448e6cef9704bb12fca8c7c4cf2610f1
onlinehk <onlinehk@gmail.com>
2019-02-08 13:43:29 +0800

    added animate transition

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/count/aa/bb">Count</router-link>
+    <div id="app">
+        <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link> |
+        <router-link to="/count/aa/bb">Count</router-link>
+        </div>
+        <transition mode="out-in"
+        enter-active-class="animated fadeIn"
+        leave-active-class="animated fadeOut">
+            <router-view/>
+        </transition>
     </div>
-    <router-view/>
-  </div>
 </template>
 
 <style lang="scss">
@@ -15,21 +19,25 @@
 @import '../node_modules/pure-css-loader/dist/css-loader.css';
 
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 #nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-      font-size: 20px;
+    padding: 30px;
+    a {
+        font-weight: bold;
+        color: #2c3e50;
+        &.router-link-exact-active {
+        color: #42b983;
+        font-size: 20px;
+        }
     }
-  }
 }
+.animated {
+    animation-duration: 0.5s;
+}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
+import animate from 'animate.css'
 import App from './App.vue'
 import router from './router'
 import store from './store/'
 
 Vue.use(BootstrapVue);
+Vue.use(animate);
 
 Vue.config.productionTip = false
 
